652cabc0aa31258676bdd5fce70e05d9ad139ca0
Bjornar97 <hetlest@hotmail.com>
2025-09-30 21:03:23 +0200

    Add icons without purpose set

--- vite.config.ts
+++ vite.config.ts
@@ -34,6 +34,16 @@ export default defineConfig({
                         type: "image/png",
                         purpose: "any maskable",
                     },
+                    {
+                        src: "web-app-manifest-192x192.png",
+                        sizes: "192x192",
+                        type: "image/png",
+                    },
+                    {
+                        src: "web-app-manifest-512x512.png",
+                        sizes: "512x512",
+                        type: "image/png",
+                    },
                 ],
             },
         }),
